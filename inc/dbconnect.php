73bfa6f275a1b29939de3abbfe46405cd947bd3f
bastolasuraj <bastolasuraj@gmail.com>
2016-05-25 14:50:31 +0545

    Update dbconnect.php

--- inc/dbconnect.php
+++ inc/dbconnect.php
@@ -1,3 +1,20 @@
-zdjakabsmn
-,dbncm
-anskjb
+<?php
+
+    if($_SERVER['REMOTE_ADDR']=="127.0.0.1"){
+        define("HOST","localhost");
+        define("USER","root");
+        define("PASS","");
+        define("DATA","javaphpcs");
+        $dbconnect=@mysqli_connect(HOST,USER,PASS,DATA);
+    }else{
+        define("HOST","10.2.0.150");
+        define("USER","sathyasai_sathya");
+        define("PASS","@#sathya#@");
+        define("DATA","sathyasai_result");
+        $dbconnect=@mysqli_connect(HOST,USER,PASS,DATA);
+    }
+    
+    
+    
+    
+?>
